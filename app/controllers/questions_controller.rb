f9de373cf1fc3a8f735c25e6f73a7073fa975d99
Pepe Baena <pepespepes@gmail.com>
2021-11-08 15:30:02 +0000

    challenge finished

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:answer]
+    if @question.downcase == "i am going to work right now!"
+      @response = ''
+    elsif @question.end_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
